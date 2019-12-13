9bd1e1cfd9bd9a9b5821a7354814f51ccb2e5af3
Lovepreet29 <54912545+Lovepreet29@users.noreply.github.com>
2019-12-12 20:27:59 -0700

    little bit

--- friend-finder/js/main.js
+++ friend-finder/js/main.js
@@ -16,5 +16,37 @@ fetch("../friends/friends.json")
         div.innerHTML = data[i].firstName;
         mainContainer.appendChild(div)
     }
- 
+    var data = [{
+        "id": 1,
+        "firstName": "Jane",
+        "lastName": "Doe",
+        "avatar": "jane_doe.jpg"
+    }, {
+        "id": 2,
+        "firstName": "Tom",
+        "lastName": "Jones",
+        "avatar": "tommy_gun.jpg"
+    }, {
+        "id": 3,
+        "firstName": "Clark",
+        "lastName": "Kent",
+        "avatar": "supes.jpg"
+    }, {
+        "id": 4,
+        "firstName": "Sally",
+        "lastName": "Anne",
+        "avatar": "sally.jpg"
+    }]
+    
+    var htmlText = data.map(function(o){
+      return `
+          <div class="div-conatiner">
+          <p class="p-name"> Name: ${o.firstName}</p>
+          <p class="p-loc"> Location: ${o.lastName}</p>
+          <p class="p-desc"> Description: ${o.avatar}</p>
+         </div>
+      `;
+    });
+    
+    $('body').append(htmlText);
 
